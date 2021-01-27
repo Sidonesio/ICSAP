609ed1b85f59d6afea6dcd14a13057917d8ca037
Sidonesio <sbissoli76@uol.com.br>
2021-01-27 11:21:01 -0300

    Agrupamentos

---
+++ SIH 04 - group.R
@@ -0,0 +1,7 @@
+
+colunas <- c("SEXO", "NASC", "COD_IDADE", "IDADE", "RACA_COR", "INSTRU", 
+             "NUM_FILHOS", "CBOR", "MUNIC_RES", "DT_INTER", "DT_SAIDA", 
+             "DIAS_PERM", "MUNIC_MOV", "DIAG_PRINC", "COMPLEX", "MORTE", 
+             "CID_MORTE", "VAL_TOT", "US_TOT")
+ColMinor <- c("SEXO", "COD_IDADE", "IDADE", "MUNIC_RES", "DT_INTER", 
+             "DIAG_PRINC", "MORTE")
