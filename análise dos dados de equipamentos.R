b940706b666a7200aa52bbf90c02c5c7c813c052
rolfregehr <rolf.regehr@gmail.com>
2021-05-13 07:09:28 -0300

    inclusão de ajuste dos valores no eixo x

--- análise dos dados de equipamentos.R
+++ análise dos dados de equipamentos.R
@@ -12,7 +12,8 @@ equip %>% group_by(tipo_equipamento, mes_numerico) %>%
   summarise(qtde=sum(QT_USO)) %>%
   ggplot(aes(x=mes_numerico, y=qtde, color=tipo_equipamento)) +
   geom_line(size=1)+
-  facet_wrap(~tipo_equipamento)
+  facet_wrap(~tipo_equipamento)+
+  scale_x_continuous(breaks = seq(1, 12, by=1))
 
 # que valor usar? média do ano, valor em algum mês de referência?
 # usar o indicador de quantidade (por tipo ou todos?)
